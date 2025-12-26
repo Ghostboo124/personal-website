6e8bd6079bf69d8fc46bd933947bbd90e2b16f83
Ghostboo124 <alexander.perkins11@gmail.com>
2025-12-27 02:20:47 +1000

    Add dark mode class switch
    Signed-off-by: Ghostboo124 <alexander.perkins11@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,16 @@ import { Button } from "@/components/button";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen items-center justify-center font-sans bg-black .dark">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-black sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-zinc-50">
             Personal Website
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+          <p className="max-w-md text-lg leading-8 text-zinc-400">
             This is Alex's (Ghostboo124) personal website
           </p>
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-zinc-50">
             Profiles
           </h1>
           <ul className="list-disc">
