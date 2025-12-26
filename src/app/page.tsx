737e4db14d6738b9216ba0410c4b023d2dc34a0f
Ghostboo124 <alexander.perkins11@gmail.com>
2025-12-27 02:43:19 +1000

    Fix dark mode implimentation
    Signed-off-by: Ghostboo124 <alexander.perkins11@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/button";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans bg-black .dark">
+    <div className="flex min-h-screen items-center justify-center font-sans bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-black sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-zinc-50">
