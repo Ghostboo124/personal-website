613a960646f8b865e10aff959698753aa2779cd4
Ghostboo124 <alexander.perkins11@gmail.com>
2026-01-08 23:50:10 +1000

    chore: update providers.tsx to be more safe
    Signed-off-by: Ghostboo124 <alexander.perkins11@gmail.com>

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -8,7 +8,19 @@ import { PostHogProvider as PHProvider } from "posthog-js/react";
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+    const key = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+    if (!key) {
+      console.warn("PostHog key not not found, analytics disabled"); 
+      return;
+    }
+
+    // Prevent re-initialisation in strict mode
+    if (posthog.__loaded) {
+      console.log("PostHog already initialised, skipping re-initialisation");
+      return;
+    }
+
+    posthog.init(key, {
       api_host:
         process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com",
       person_profiles: "always", // or 'identified_only' to create profiles for only identified users
