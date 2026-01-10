c77f29bea902f17e849316c979ca1ff8bc0fa467
Ghostboo124 <alexander.perkins11@gmail.com>
2026-01-10 12:10:51 +1000

    fix: custom-link not using forwardRef
    Signed-off-by: Ghostboo124 <alexander.perkins11@gmail.com>

--- src/components/custom-link.tsx
+++ src/components/custom-link.tsx
@@ -1,8 +1,35 @@
 import clsx from "clsx";
 import NextLink from "next/link";
+import { forwardRef, ComponentProps } from "react";
 
-export default function Link({
-  ...props
-}: React.ComponentProps<typeof NextLink>) {
-  return <NextLink {...props} className={clsx("underline", props.className)} />;
-}
+const Link = forwardRef<HTMLAnchorElement, ComponentProps<typeof NextLink>>(
+  ({ href, className, ...props }, ref) => {
+    const isExternal =
+      typeof href === "string" &&
+      (href.startsWith("http://") || href.startsWith("https://"));
+
+    if (isExternal) {
+      return (
+        <a
+          ref={ref}
+          href={href}
+          className={clsx("underline", className)}
+          {...props}
+        />
+      );
+    }
+
+    return (
+      <NextLink
+        ref={ref}
+        href={href}
+        className={clsx("underline", className)}
+        {...props}
+      />
+    );
+  }
+);
+
+Link.displayName = "Link";
+
+export default Link;
