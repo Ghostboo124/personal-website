a574bca27769d33cb400af6bc6c88b073700d0ec
Ghostboo124 <alexander.perkins11@gmail.com>
2026-01-22 16:53:29 +1000

    feat: add vercel speedinsights
    Signed-off-by: Ghostboo124 <alexander.perkins11@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { SmoothCursor } from "@/components/smooth-cursor";
 import { ConvexClientProvider, PostHogProvider } from "./providers";
 
@@ -40,6 +41,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SpeedInsights />
         <PostHogProvider>
           <ConvexClientProvider>
             {children}
