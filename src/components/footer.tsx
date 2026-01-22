2e44b5a082e136c013d02f6c68027e5697f30ba4
Ghostboo124 <alexander.perkins11@gmail.com>
2026-01-22 16:57:02 +1000

    fix: contrast of footer "Built with" text
    Signed-off-by: Ghostboo124 <alexander.perkins11@gmail.com>

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <footer className="flex-none p-4 w-full bg-ctp-mantle text-ctp-text text-center">
       <div className="flex flex-col gap-2 text-sm text-ctp-subtext0">
         <p>© 2026 Lexy</p>
-        <p className="text-xs text-ctp-overlay0">
+        <p className="text-xs text-ctp-subtext0">
           Built with Next.js, Tailwind CSS, and Shadcn UI with PostHog for
           analytics.
         </p>
