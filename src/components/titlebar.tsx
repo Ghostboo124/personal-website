561ecbf12a4b9d635492f603728fa26af1540bc6
Ghostboo124 <alexander.perkins11@gmail.com>
2026-01-23 12:01:47 +1000

    fix: add login button in the titlebar
    Signed-off-by: Ghostboo124 <alexander.perkins11@gmail.com>

--- src/components/titlebar.tsx
+++ src/components/titlebar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Button } from "./button";
 
 export function Titlebar() {
   return (
@@ -7,8 +8,17 @@ export function Titlebar() {
         <Link
           href="/"
           className="text-lg font-semibold text-ctp-text hover:text-ctp-mauve transition-colors"
+          aria-label="Link back to home page saying 'Lexy'"
         >
           Lexy
+        </Link>
+        <Link href="/auth" aria-label="Link to auth page saying 'Login'">
+          <Button
+            variant="outline"
+            className="text-lg bg-ctp-mantle text-ctp-mauve"
+          >
+            Login
+          </Button>
         </Link>
       </nav>
     </header>
